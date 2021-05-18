86d6b283c32f0662afd1fd5c8ea6135f31994a57
murtalahabeeb <murtalahabeeb@gmail.com>
2021-05-18 23:33:43 +0100

    tested something in web

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'welcome';
 });
 use Illuminate\Http\Request;
 use App\Http\Controllers\LocationController;
