4c56a262899188123be51f0b19f7cc8e3493c681
murtalahabeeb <murtalahabeeb@gmail.com>
2021-06-02 17:49:29 +0100

    included facades db

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Http\Controllers\DB;
 use App\Models\Location;
 use App\Models\Room;
 use App\Models\User;
